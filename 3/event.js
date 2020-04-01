3460a2f6f53034026aef9c6a9de0cece3c98aaf7
Mohammad amrollahi <52289677+mohammadamrollahi@users.noreply.github.com>
2020-04-02 00:39:08 +0430

    finish no.3

---
+++ 3/event.js
@@ -0,0 +1,16 @@
+/*function myFlat(arr)
+{
+    return arr.flat(Infinity)
+}
+console.log(myFlat([5,7,8,[2,1]]))
+*/
+function myFlat(arr) {
+
+    return arr.reduce(myFunc,[])
+    function myFunc(totla,item){
+    
+         return total.concat(Array.isArray(item)?myFlat(item):item)
+    }
+
+  }
+
