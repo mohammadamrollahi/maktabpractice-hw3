6a07d859b617968c885063dc60e8344dfd57512e
Mohammad amrollahi <52289677+mohammadamrollahi@users.noreply.github.com>
2020-04-01 20:58:05 +0430

    finish no.6

--- 6/event.js
+++ 6/event.js
@@ -1,5 +1,21 @@
-function myExtract(myString)
+/*function myExtract(myString)
 {
     return myString.match(/(\d+)/g)
+}
+console.log(myExtract("i have 2 apples and 3 pineapples"))
+*/
+function myExtract(myString)
+{
+    let nums=[]
+    let counter=0;
+    for(let i=0;i<myString.length;i++)
+    {
+        if ((myString[i])/myString[i]==1)
+        {
+            nums[counter]=myString[i]
+            counter++;
+        }
+    }
+    return nums
 }
 console.log(myExtract("i have 2 apples and 3 pineapples"))
